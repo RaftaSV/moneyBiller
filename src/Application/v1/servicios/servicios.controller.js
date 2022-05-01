94b563fe8c3e91aa0073d624ef3532553a6177c2
RaftaSV <62962896+RaftaSV@users.noreply.github.com>
2022-05-01 16:02:07 -0600

    module services ready

--- src/Application/v1/servicios/servicios.controller.js
+++ src/Application/v1/servicios/servicios.controller.js
@@ -2,9 +2,10 @@ import serviceModule from './servicios.model';
 
 export const getAllServices = async (req, res) => {
   const { offset, limit } = req.params;
+  const { status = 'active' } = req.query;
 
   try {
-    const data = await serviceModule.find().skip(offset).limit(limit);
+    const data = await serviceModule.find({ status, }).skip(offset).limit(limit);
     return res.status(200).json(data);
   } catch (error) {
     console.error(error);
@@ -85,6 +86,64 @@ export const createService = async (req, res) => {
     return res.status(500).json({
       message: 'Error al obtener los datos',
       code: 500,
+    });
+  }
+};
+
+export const updateService = async (req, res) => {
+  const { body, params } = req;
+  const { idService } = params;
+
+  if (!body) {
+    return res.status(400).json({
+      message: 'Faltan datos, la consulta debe contener',
+      code: 400,
+    });
+  }
+  try {
+    const data = await serviceModule.findOneAndUpdate(
+      { _id: idService },
+      {
+        serviceName: body.serviceName,
+        invoiceUrl: body.invoiceUrl,
+        invoiceMethod: body.invoiceMethod,
+        numberInvoice: body.numberInvoice,
+        nameInvoiceJSON: body.nameInvoiceJSON,
+        nameInvoiceDateJSON: body.nameInvoiceDateJSON,
+        nameInvoiceTotalJSON: body.nameInvoiceTotalJSON,
+        payInvoiceUrl: body.payInvoiceUrl,
+        payInvoiceMethod: body.payInvoiceMethod,
+        DocumentInvoice: body.DocumentInvoice,
+        companyId: body.companyId,
+        status: body.status
+      }
+    );
+    return res.status(200).json(Object.assign(data, body));
+  } catch (error) {
+    return res.status(500).json({
+      code: 500,
+      message: 'No se pudo actualizar el registro',
+    });
+  }
+};
+
+export const deleteService = async (req, res) => {
+  const { params } = req;
+  const { idService } = params;
+  try {
+    const data = await serviceModule.findOneAndUpdate(
+      { _id: idService },
+      { status: 'inactive' }
+    );
+
+    return res.status(200).json({
+      ...data,
+      status: 'inactive'
+    });
+  } catch (error) {
+    return res.status(500).json({
+      code: 500,
+      message: 'No se pudo eliminar el servicio',
     });
   }
 };
--- src/Application/v1/servicios/servicios.route.js
+++ src/Application/v1/servicios/servicios.route.js
@@ -3,11 +3,14 @@ import {
   getAllServices,
   createService,
   getServiceByCompany,
+  updateService,
+  deleteService
 } from './servicios.controller';
 
 const router = express.Router();
 router.get('/', getAllServices);
 router.get('/:companyId', getServiceByCompany);
 router.post('/', createService);
-
+router.put('/:idService', updateService);
+router.delete('/:idService', deleteService);
 export default router;
